92c3ef4a2c0ceb179a66e7468ce6c76399b17f63
Marci Clark <marcig612@gmail.com>
2019-01-21 21:28:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,6 @@ def batch_badge_creator
   speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   puts "Hello, my name is #name"
 end
+
+def assign_rooms
+  rooms = [1,2,3,4,5,6,7]
